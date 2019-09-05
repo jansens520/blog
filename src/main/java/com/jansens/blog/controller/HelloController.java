fa8d76a9107c661e82e4da5e91951024cde3b867
user.name <2251478696@qq.com>
2019-09-05 18:40:06 +0800

    我的博客

---
+++ src/main/java/com/jansens/blog/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.jansens.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value ="hello")
+    public Object hello(){
+        return "Hello Blog";
+    }
+}
